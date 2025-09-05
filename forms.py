1510897fbb407e1fc8730b16f18b9012e64319f9
Maroyan Putros <maroyanputros@Maroyans-MacBook-Air.local>
2025-09-05 15:47:53 -0400

    fixed added in new form for admin sign in

---
+++ forms.py
@@ -0,0 +1,34 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo,  Length, Regexp
+# from models import Vehicle
+
+class ContactUsForum(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    phone = StringField('Phone Number', validators=[
+        Length(min=7, max=20, message="Please enter a valid phone number."),
+        Regexp(r'^[\d\-\+\(\) ]+$', message="Invalid phone number format.")
+    ])
+    message = StringField('Message', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class VehicleForm(FlaskForm):
+    make = StringField('Make', validators=[DataRequired()])
+    model = StringField('Model', validators=[DataRequired()])
+    year = StringField('Year', validators=[DataRequired()])
+    price = StringField('Price', validators=[DataRequired()])
+    
+    image = FileField('Vehicle Image', validators=[
+        FileRequired(),
+        FileAllowed(['jpg', 'jpeg', 'png', 'gif'], 'Images only!')
+    ])
+    
+    submit = SubmitField('Add Vehicle')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    # remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
